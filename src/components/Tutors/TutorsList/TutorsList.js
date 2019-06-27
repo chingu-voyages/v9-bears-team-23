5e6cd7e6c02d10cd747fd3244bd2138e192c6654
Matthew Hargraves <matthew.hargraves@cef.co.uk>
2019-06-27 21:02:54 +0100

    data in and basic info shown

--- src/components/Tutors/TutorsList/TutorsList.js
+++ src/components/Tutors/TutorsList/TutorsList.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from "axios";
-import AdCard from '../../common/AdCard/AdCard'
+
 // import SearchPanel from '../../common/AdCard/SearchPanel';
 import AdFilters from '../../common/AdFilters/AdFilters'
 
@@ -11,51 +11,39 @@ import {
   Container
 
 } from "reactstrap";
+import TutorCard from "../../common/TutorCard/TutorCard";
 
 export default function TutorsList() {
   const [tutors,setTutors] = useState([]);
-  // useEffect(() => {
-  //   fetch(`http://0.0.0.0:3005/tutor/`)
-  //     .then(results => results.json())
-  //     .then(setTutors(results.data))
-  //
-  // }, []);
-
   useEffect(async () => {
     const result = await axios(
       'http://0.0.0.0:3005/tutor/',
     );
-
-    setTutors(result.data);
+    setTutors(result.data.data);
   }, []);
-
+  // const [tutors,setTutors] = useState([]);
+  // useEffect(() => {
+  //   axios.get('http://0.0.0.0:3005/tutor/')
+  //     .then(resp => {
+  //       setTutors(resp.data.data);
+  //     });
+  // }, []);
 
   return (
-
     <Container className="container py-5">
-      <pre>{JSON.stringify(tutors, null, 2)}</pre>
+      {/*<Row>*/}
+      {/*  <Col sm="8">*/}
+      {/*    <AdFilters />*/}
+      {/*  </Col>*/}
+      {/*</Row>*/}
+      {/*<Row>*/}
+      {/*  <Col sm="12">*/}
+      {/*    <h1>Hello</h1>*/}
+      {/*    /!*<SearchPanel />*!/*/}
+      {/*  </Col>*/}
+      {/*</Row>*/}
       <Row>
-        <Col sm="8">
-          <AdFilters />
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="12">
-          <h1>Hello</h1>
-          {/*<SearchPanel />*/}
-        </Col>
-      </Row>
-      <Row>
-        <Col sm="4">
-          <AdCard />
-        </Col>
-        <Col sm="4">
-          <AdCard />
-        </Col>
-        <Col sm="4">
-          <AdCard />
-        </Col>
-
+        { tutors.map(tutor => <TutorCard tutor={tutor} />) }
       </Row>
     </Container>
   )
---
+++ src/components/common/TutorCard/TutorCard.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import StarRating from '../StarRating/StarRating'
+
+// reactstrap components
+import {
+  Card,
+  CardBody,
+  CardImg,
+  CardTitle,
+
+} from "reactstrap";
+import './style.scss'
+
+class TutorCard extends React.Component {
+
+  render(){
+    const {firstName, lastName, rating} = this.props.tutor;
+    return (
+      <Card className="card" >
+        <CardBody>
+          <section className="d-flex flex-row align-items-center">
+            <CardImg className="rounded-circle" style={{ width: "70px", height: "70px" }} src="https://via.placeholder.com/50" alt="Card image cap" />
+            <CardTitle className="mt-3 pl-5 card-title">{firstName} {lastName}</CardTitle>
+          </section>
+          <section className="star-rating d-flex flex-row justify-content-start pt-4">
+            <StarRating
+              totalStars={rating} />
+          </section>
+          <section className="details-page pt-5 pb-3">
+            <Link className="detail-link" style={{ color: 'white' }} to="/ads/:id">Ad type <i class="ni ni-bold-right"></i></Link>
+          </section>
+          <section className="ad-info d-flex flex-row">
+            <div className="location text-white px-1 d-flex flex-row justify-content-center align-items-center">
+              <span className="pr-2">Mumbai</span><span><i class="fa fa-map-marker"></i></span>
+            </div>
+            <div className="price text-white px-1 d-flex flex-row justify-content-center align-items-center">
+              <span>&#x00024;</span><span>82</span>
+            </div>
+          </section>
+        </CardBody>
+      </Card>
+    )
+  }
+}
+export default TutorCard;
